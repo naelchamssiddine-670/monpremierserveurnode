563e0fd9450e8e4c252058d2233d1ca4cd7fb121
naelchamssiddine-670 <naelchamssiddine@gmail.com>
2026-01-26 10:57:11 +0300

    l'utilisation d'un serveur

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const express = require('express');
 // J'initialise mon application Express.
 const app = express();
 
-
+// Je définis un middleware qui intercepte toutes les requêtes entrantes.
 app.use((req, res, next) => {
     res.status(200).json({
-        message: 'La requête a bien été reçue !'
+        message: 'Bonjour, la requête a bien été prise en compte !'
     });
+    next();
+    });
+// Je définis un autre middleware pour illustrer le fonctionnement des middlewares.
+app.use((req, res) => {
+    res.json('Je suis un middlware dans le serveur !');
 });
 
-// J'exporte l'application express sous forme de module. ainsi, l'application peut être importée dans d'autres fichiers JS.
+// Je définis un dernier middleware pour répondre aux requêtes.
+app.use((req, res) => {
+    console.log('Vous êtes bien sur le serveur de Chamssiddine!');
+    });
+
+    
+// J'exporte l'application express sous forme de module. ainsi, l'application peut être importée dans d'autres.
 module.exports = app;
